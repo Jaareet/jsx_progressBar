b17b72741049142a7a3cf5f06fa9e0ae72af25c0
Jaareet <jaareet.soporte@gmail.com>
2024-01-05 13:23:49 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,12 +1,16 @@
-local isProgressBarOpened = false
+local isProgressBarOpened, callback = false, nil
 
 RegisterNUICallback('finish', function()
     if (isProgressBarOpened) then
         isProgressBarOpened = false
+
+        if(not callback) then return end
+        callback()
+        callback = nil
     end
 end)
 
-function startProgressBar(text, timeouts)
+function startProgressBar(text, timeouts, cb)
     if (isProgressBarOpened) then
         return
     end
@@ -18,6 +22,9 @@ function startProgressBar(text, timeouts)
         text = text,
         timeouts = timeouts,
     })
+
+    if(not cb) then return end
+    callback = cb
 end
 
 exports('startProgressBar', startProgressBar)
@@ -26,6 +33,13 @@ RegisterNetEvent('jsx_progressBar:client:startProgressBar', startProgressBar)
 
 if GetConvarInt('jsx_progressBar:test', 0) == 1 then
     RegisterCommand('test', function()
-        startProgressBar('Hola', 6000)
+        startProgressBar('Hola', 6000, function()
+            TriggerEvent('chat:addMessage', {
+                args = {
+                    'SISTEMA',
+                    'Terminado'
+                }
+            })
+        end)
     end)
 end
